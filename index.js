7ca0c2b45acbbe34f0b6c821e25edf4bca6f2475
Shivam Kumar Kashyap <109240141+MernWala@users.noreply.github.com>
2024-08-06 00:34:05 +0530

    expanding whitelist

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const whitelist = ['http://localhost:3000'];
+const whitelist = ['http://localhost:3000', 'https://keepplus.netlify.app/'];
 
 app.use(express.json());
 app.use(cookieParser());
